461f7e35ff0ec275ff55e40eb1c500b3aa5c1229
Emi0689 <emiliofilotti@gmail.com>
2025-03-07 19:47:00 -0300

    Add swaggerUI

--- SalesSystem.API/Program.cs
+++ SalesSystem.API/Program.cs
@@ -15,12 +15,8 @@ builder.Services.AddCors(op => { op.AddPolicy("Policy", app => app.AllowAnyHeade
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("Policy");
 
